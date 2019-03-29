9dc967a99a9f93bec521d93d10f95d034866c833
Shivam Mishra <shivam.g.mishra@gmail.com>
2019-03-29 20:27:16 +0530

    adding projects CRUD operations

---
+++ inventoryMgmt/Controllers/ProjectsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using inventoryMgmt.Models;
+
+namespace inventoryMgmt.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private inventoryMgmtPocEntities db = new inventoryMgmtPocEntities();
+
+        // GET: Projects
+        public async Task<ActionResult> Index()
+        {
+            var projects = db.Projects.Include(p => p.Team);
+            return View(await projects.ToListAsync());
+        }
+
+        // GET: Projects/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = await db.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // GET: Projects/Create
+        public ActionResult Create()
+        {
+            ViewBag.team_id = new SelectList(db.Teams, "id", "name");
+            return View();
+        }
+
+        // POST: Projects/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,name,team_id")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(project);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.team_id = new SelectList(db.Teams, "id", "name", project.team_id);
+            return View(project);
+        }
+
+        // GET: Projects/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = await db.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.team_id = new SelectList(db.Teams, "id", "name", project.team_id);
+            return View(project);
+        }
+
+        // POST: Projects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,name,team_id")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.team_id = new SelectList(db.Teams, "id", "name", project.team_id);
+            return View(project);
+        }
+
+        // GET: Projects/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = await db.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Project project = await db.Projects.FindAsync(id);
+            db.Projects.Remove(project);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
