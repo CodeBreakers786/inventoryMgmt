35cb50d319b7d596bf7fcd7ccaf36dc3a7698f92
Shivam Mishra <shivam.g.mishra@gmail.com>
2019-03-29 19:39:50 +0530

    Add project files.

---
+++ inventoryMgmt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="inventoryMgmt.MvcApplication" Language="C#" %>
---
+++ inventoryMgmt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(inventoryMgmt.Startup))]
+namespace inventoryMgmt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
