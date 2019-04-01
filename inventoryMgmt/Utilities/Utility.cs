b68cc4ad14dee9ec4feae018ffdfbd18f1c8f0c1
Shivam Mishra <shivam.g.mishra@gmail.com>
2019-04-01 11:55:46 +0530

    adding bootstrap 4.3.1 js packages

---
+++ inventoryMgmt/Utilities/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace inventoryMgmt.Utilities
+{
+    public class Utility
+    {
+        public static string getUniqueKey()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
